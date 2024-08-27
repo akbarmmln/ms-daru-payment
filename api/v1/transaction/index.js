4f29f022e5e3dd516c7d40d839297d243fb009ff
Muhammad Taufik <muhammad.taufik@adira.co.id>
2024-08-28 00:10:29 +0700

    update code

--- api/v1/transaction/index.js
+++ api/v1/transaction/index.js
@@ -6,6 +6,6 @@ const utils = require('../../../utils/utils');
 router.post('/create-va', controller.createVa);
 
 router.get('/va-info', utils.verifyTokenMs, controller.vaInfo);
-router.get('/transfer/inquiry', utils.verifyTokenMs, controller.transferInquiry);
+router.post('/transfer/inquiry', utils.verifyTokenMs, controller.transferInquiry);
 
 module.exports = router;
