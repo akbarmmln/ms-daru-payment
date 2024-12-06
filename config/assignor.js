890d1881984d81d44396aec2abd5d04be4c472a0
Muhammad Taufik <muhammad.taufik@adira.co.id>
2024-12-06 07:28:52 +0700

    update code

--- config/assignor.js
+++ config/assignor.js
@@ -45,8 +45,6 @@ class Assignor {
         const namespace = process.env.NAMESPACE;
         const response = await this.client.listNamespacedPod(namespace, undefined, undefined, undefined, undefined, `app=${process.env.SELECTOR}`);
         const pods = response.body.items;
-        logger.infoWithContext(`response available pods in namespace ${namespace} with selector ${process.env.SELECTOR}: ${JSON.stringify(pods)}`)
-
         const currentPod = this.getIdentifier();
         if (currentPod === "") {
           throw new Error("Invalid POD name - Unset");
